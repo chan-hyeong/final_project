32b1f83adf6ef7fb17a0edd7e1c87669254ed6cc
MSDN-SPECIAL\Administrator <cherryae11@gmail.com>
2017-05-19 18:23:02 +0900

    order_detail 수정 & shoppingbag 하는중

--- FinalProject_Customer/src/main/java/kosta/finalproject/customer/controller/CustomerController.java
+++ FinalProject_Customer/src/main/java/kosta/finalproject/customer/controller/CustomerController.java
@@ -1,5 +1,9 @@
 package kosta.finalproject.customer.controller;
 
+import java.util.Enumeration;
+import java.util.List;
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
@@ -12,7 +16,11 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import kosta.finalproject.customer.dao.CustomersDAO;
 import kosta.finalproject.customer.dao.MenuDAO;
+import kosta.finalproject.customer.dao.Order_DetailDAO;
+import kosta.finalproject.customer.dao.Order_ListDAO;
 import kosta.finalproject.customer.dto.CustomersTDTO;
+import kosta.finalproject.customer.dto.Order_DetailTDTO;
+import kosta.finalproject.customer.dto.Order_DetailTDTO_basket;
 
 @Controller
 public class CustomerController {
@@ -121,10 +129,20 @@ public class CustomerController {
 	}
 		
 	@RequestMapping("/payment.do")
-	public String menudetail(/*HttpServletRequest request,@RequestParam("m_code") String m_code*/) {
-		/*MenuDAO dao=sqlsession.getMapper(MenuDAO.class);
-		request.setAttribute("menudto", dao.menudetail(m_code));
-		request.setAttribute("option",dao.menuoption());*/
+	public String menudetail(HttpServletRequest request,Order_DetailTDTO dto) {
+		
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+		/*Map paramMap = request.getParameterMap();		
+		String[] vege = (String[]) paramMap.get("vege");
+		for(int i=0;i<vege.length;i++){
+			System.out.println(vege[i]);
+		}*/
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+		String command = request.getParameter("command");
+		if(command!=null&&command.equals("basket")){
+			
+			return "redirect:shoppingbag.do";
+		}
 		return "customer/paymentform";
 	}
 
@@ -133,13 +151,25 @@ public class CustomerController {
 		MenuDAO dao=sqlsession.getMapper(MenuDAO.class);
 		request.setAttribute("menudto", dao.menudetail(m_code));
 		request.setAttribute("option",dao.menuoption());
-
 		return "customer/orderdetail";
 	}
 
 	@RequestMapping("/shoppingbag.do")
-	public String shoppingbag() {
-
+	public String shoppingbag(HttpServletRequest request) {		
+		HttpSession session = request.getSession();
+		String ID = (String) session.getAttribute("id");
+		if(ID==null) return "customer/loginform";
+		
+		String command = request.getParameter("command");
+		Order_DetailDAO dao=sqlsession.getMapper(Order_DetailDAO.class);	
+		if(command!=null&&command.equals("delete")){
+			int order_num1=Integer.parseInt(request.getParameter("order_num"));
+			dao.order_detail_delete1(order_num1);
+			dao.order_detail_delete2(order_num1);	
+			return "redirect:shoppingbag.do";
+		}
+			
+		request.setAttribute("order_detail", dao.order_detail_list(ID));
 		return "customer/shoppingbag";
 	}
 }
